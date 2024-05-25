80e5baab381c561de3b122bd0361d807b444eee7
Gabriel Guedes Dos Santos <paulo.guedes.santos7@gmail.com>
2024-05-25 17:50:07 -0300

    Alterando a conexão do banco

--- src/Data/connection.ts
+++ src/Data/connection.ts
@@ -1,16 +1,21 @@
 import knex from "knex";
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const connection = knex({
-    client: "mysql",
+    client: "mysql2",
     connection: {
         host: process.env.DB_HOST,
         port: Number(process.env.DB_PORT || "3306"),
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        ssl: {
+            ca: fs.readFileSync(path.resolve(__dirname, "../../", process.env.DB_CA_PATH)),
+        },
     },
 });
 
